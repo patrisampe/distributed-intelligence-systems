8d22c237081142daae23db75a85723edd68147c3
patricia.sampedro <patricia.sampedro@est.fib.upc.edu>
2016-06-13 22:56:07 +0200

    funcions fetes

--- src/practica/Methods.java
+++ src/practica/Methods.java
@@ -27,4 +27,67 @@ public class Methods {
 	}
 	
 	
+	public long calculateTime(WaterMass wm, TreatmentPlant tp, Permisions p){
+		
+		long time = 0;		
+		for( Pollutant po:wm.getPollutants() ){
+			
+		    Double allowed =p.amountPollutant(po.getType());
+		    		
+		    Double unitpertime=tp.amountPollutant(po.getType());
+		    
+		    Double diff= po.getAmount() - allowed;
+		    
+		    if(diff>0){
+		    	long timeaux=Math.round(diff/unitpertime);
+		    	
+		    	if(timeaux>time){
+		    		time=timeaux;   		
+		    	}
+		    }
+		}
+		return time;
+	}
+	
+	
+	public WaterMass depureMass(WaterMass wm, TreatmentPlant tp, Permisions p,long existanceTime){
+		
+		
+		long time = calculateTime(wm,tp,p);	
+		
+		Collection<Pollutant> poNew = null;
+		
+		for( Pollutant po:wm.getPollutants() ){
+			
+		    		
+		    Double unitpertime=tp.amountPollutant(po.getType());
+		    Double newAmount= po.getAmount() - unitpertime*time;
+		    
+		    Pollutant newPo = new Pollutant(po.getUnit(),po.getType(),newAmount);
+		    
+		    poNew.add(newPo);
+		    
+		}
+		Vector<WaterMass> originMass = wm.getOriginMass();
+		originMass.add(wm);
+		return new WaterMass(poNew,originMass, wm.getLiters(),existanceTime,tp);
+	}
+	
+	
+	public boolean permited(WaterMass wm, Permisions p){
+		
+		
+	
+		for( Pollutant po:wm.getPollutants() ){
+			
+		    Double allowed =p.amountPollutant(po.getType());
+		    
+		    if(allowed < po.getAmount())return false;
+		    
+		}
+		return true;
+	}
+	
+	
+	
 }
--- src/practica/objects/TreatmentPlant.java
+++ src/practica/objects/TreatmentPlant.java
@@ -2,23 +2,29 @@ package practica.objects;
 
 import java.util.LinkedHashMap;
 
-public class TreatmentPlant {
-	LinkedHashMap<pollutantTypes,Integer> pe = new LinkedHashMap<>(); //pollution eliminate per unit time
+public class TreatmentPlant extends Localization{
+	LinkedHashMap<pollutantTypes,Double> pe = new LinkedHashMap<>(); //pollution eliminate per unit time
 
-	public LinkedHashMap<pollutantTypes, Integer> getPe() {
+	public LinkedHashMap<pollutantTypes, Double> getPe() {
 		return pe;
 	}
 
-	public void setPe(LinkedHashMap<pollutantTypes, Integer> pe) {
+	public void setPe(LinkedHashMap<pollutantTypes, Double> pe) {
 		this.pe = pe;
 	}
 
-	public TreatmentPlant(LinkedHashMap<pollutantTypes, Integer> pe) {
+	public TreatmentPlant(LinkedHashMap<pollutantTypes, Double> pe) {
 		this.pe = pe;
 	}
 	
-	public void addType(pollutantTypes pt, Integer i){
+	public void addType(pollutantTypes pt, Double i){
 		this.pe.put(pt, i);
+		
+	}
+	
+	public Double amountPollutant(pollutantTypes pt){
+		
+		return pe.get(pt);		
 		
 	}
 	
