a0f16ef71b6842669f5c2f2940543d4a9b8167a0
patricia.sampedro <patricia.sampedro@est.fib.upc.edu>
2016-06-13 12:48:29 +0200

    'riu'

--- src/practica/objects/River.java
+++ src/practica/objects/River.java
@@ -3,4 +3,20 @@ package practica.objects;
 public class River extends Localization {
 	String name;
 	int km;
+	public River(String name, int km) {
+		this.name = name;
+		this.km = km;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKm() {
+		return km;
+	}
+	public void setKm(int km) {
+		this.km = km;
+	}
 }
