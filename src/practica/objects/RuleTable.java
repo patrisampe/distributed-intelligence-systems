ff82c9a82f80c3d94b1fea3e347c1e01123cf012
Miquel <miqueljubert@gmail.com>
2016-06-17 09:52:41 +0200

    joder

--- src/practica/objects/RuleTable.java
+++ src/practica/objects/RuleTable.java
@@ -6,29 +6,29 @@ public class RuleTable {
 
 	String id;
 	
-	LinkedHashMap<String,Double> maxAllowed = new LinkedHashMap<>();
+	LinkedHashMap<String,Pollutant> maxAllowed = new LinkedHashMap<>();
 
 	public String getId()
 	{
 		return id;
 	}
 
-	public LinkedHashMap<String, Double> getMaxAllowed() {
+	public LinkedHashMap<String, Pollutant> getMaxAllowed() {
 		return maxAllowed;
 	}
 
-	public void setMaxAllowed(LinkedHashMap<String, Double> allowed) {
+	public void setMaxAllowed(LinkedHashMap<String, Pollutant> allowed) {
 		this.maxAllowed = allowed;
 	}
 
 	public Double getMaxAmountPollutant(String pt){
 		
-		return maxAllowed.get(pt);		
+		return maxAllowed.get(pt).getAmount();		
 	}
 
 	public Boolean compliant( Pollutant p ) {
 		
-		if( ( this.maxAllowed.containsKey(p.getType())) && (p.getAmount() < this.maxAllowed.get(p.getType())))
+		if( ( this.maxAllowed.containsKey(p.getType())) && (p.getAmount() < this.getMaxAmountPollutant(p.getType())))
 			return true;
 		return false;
 	}
